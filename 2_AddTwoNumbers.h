2ae8e8487cb9e480548841d20114ef2b82e5adb0
DevinChang <DevinChang@126.com>
2017-08-21 19:31:42 +0800

    2.Add Two Numbers

--- 2_AddTwoNumbers.h
+++ 2_AddTwoNumbers.h
@@ -12,23 +12,24 @@ Output: 7 -> 0 -> 8
 struct ListNode {
 	int val;
 	ListNode *next;
-	ListNode(int x) : val(0), next(nullptr){}
+	ListNode(int x) : val(x), next(nullptr){}
 };
 
 
 ListNode *addTwoNumbers(ListNode* l1, ListNode* l2) {
 	ListNode *p1 = l1, *p2 = l2;
-	ListNode *p;
+	ListNode phead = ListNode(0);
+	ListNode *p = &phead;
 	int carry = 0;
-	while (p1 != nullptr || p2 != nullptr) {
+	while (p1 != nullptr || p2 != nullptr || carry) {
 		int x = (p1 != nullptr) ? p1->val : 0;
 		int y = (p2 != nullptr) ? p2->val : 0;
 		int digit = carry + x + y;
 		carry = digit / 10;
-		p = new ListNode(digit % 10);
+		p->next = new ListNode(digit % 10);
 		p = p->next;
-		l1 = (l1 != nullptr) ? l1->next : l1;
-		l2 = (l2 != nullptr) ? l2->next : l2;
+		if (p1 != nullptr) p1 = p1->next;
+		if (p2 != nullptr) p2 = p2->next;
 	}
-	return p;
+	return phead.next;
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include "1_TwoSum.h"
-
+#include "2_AddTwoNumbers.h"
 
 
 int main() {
 	//1.twosum
+	/*
 	std::vector<int> numbers{ 3, 3};
 	int target = 6;
 	auto result = twosum(numbers, target);
@@ -11,6 +12,18 @@ int main() {
 	for (auto p : result)
 		std::cout << p << " ";
 	std::cout << std::endl;
+	*/
+
+	//2.add two numbers
+	ListNode *lhead = new ListNode(9), *l1 = new ListNode(9), *l2 = new ListNode(9);
+	lhead->next = l1;
+	l1->next = l2;
+	ListNode *rhead = new ListNode(1);
+	auto rl = addTwoNumbers(lhead, rhead);
+	while (rl) {
+		std::cout << rl->val << "-> ";
+		rl = rl->next;
+	}
 	system("pause");
 	return 0;
 }

673aec62305aeee0ff57ce048bae5686b4268a74
devinchang <devinchang@126.com>
2020-10-12 14:16:23 +0800

    feat(lc213.cpp): rewrite LC213 solution

--- LeetCode/LC213.cpp
+++ LeetCode/LC213.cpp
@@ -17,7 +17,7 @@ int myrob(vector<int>& nums){
 }
 
 
-int rob(vector<int> &nums){
+int rob_old(vector<int> &nums){
     if(nums.size() < 1)
         return 0;
     if(nums.size() == 1)
@@ -30,6 +30,25 @@ int rob(vector<int> &nums){
 }
 
 
+int rob(vector<int> &nums){
+    int n = nums.size();
+    if(!n) return 0;
+    if(n == 1) return nums[0];
+    vector<int> f(n+1);
+    vector<int> g(n+1);
+    for(int i = 2; i <= n; i++){
+        f[i] = g[i-1] + nums[i-1];
+        g[i] = max(g[i-1], f[i-1]);
+    }
+    int ret = max(f[n], g[n]);
+    f[1] = nums[0];
+    g[1] = INT_MIN;
+    for(int i = 2; i <= n; i++){
+        f[i] = g[i-1] + nums[i-1];
+        g[i] = max(g[i-1], f[i-1]);
+    }
+    return max(ret, g[n]);
+}
 
 int main(){
     string line;

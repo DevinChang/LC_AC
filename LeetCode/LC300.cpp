a88f26359adaf8b6de23cf8fd6224bd95b4df1a8
devinchang <devinchang@126.com>
2020-11-03 10:16:59 +0800

    feat(ls300): add lc300 nlogn solution

--- LeetCode/LC300.cpp
+++ LeetCode/LC300.cpp
@@ -22,6 +22,27 @@ int lengthOfLIS(vector<int> &nums){
 }
 
 
+int lengthOfLIS(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> q;
+    for(auto x : nums){
+        if(q.empty() || x > q.back()) q.push_back(x);
+        else {
+            if(x <= q[0]) q[0] = x;
+            else {
+                int l = 0, r = q.size()-1;
+                while(l < r){
+                    int mid = (l + r+1) / 2;
+                    if(q[mid] < x) l = mid;
+                    else r = mid - 1;
+                }
+                q[r+1] = x;
+            }
+        }
+    }
+    return q.size();
+}
+
 int main(){
     string line;
     while(getline(cin, line)){

66afab25c3ad87991287b400e66486035e47efb1
devinchang <devinchang@126.com>
2021-03-10 10:25:44 +0800

    feat(lc559): modify lc559

---
+++ LeetCode/LC559.cpp
@@ -0,0 +1,29 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    int maxDepth(Node* root) {
+        if(root == nullptr)  return 0;
+        int res = 0;
+        for(auto c : root->children) res = max(res, maxDepth(c));
+        return res + 1;
+    }
+};

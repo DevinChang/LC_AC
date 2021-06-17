ba8d2f3268ba2f0a8e0c893413b11c8a6748e95f
devinchang <devinchang@126.com>
2021-06-17 09:48:36 +0800

    feat(20210617): add lc111 & ac3

--- LeetCode/LC111.cpp
+++ LeetCode/LC111.cpp
@@ -2,7 +2,27 @@
 // Created by devinchang on 2019/12/13.
 //
 
-#include "util.h"
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+		if(root == nullptr) return 0;
+		if(root->left == nullptr && root->right == nullptr) return 1;
+		if(root->left && root->right) return min(minDepth(root->left), minDepth(root->right)) + 1;
+		if(root->left) return minDepth(root->left) + 1;
+		return minDepth(root->right) + 1;
+    }
+};
 
 
 

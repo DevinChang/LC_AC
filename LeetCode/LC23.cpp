7400b8c5c3ec27142c11b05736946c28fc1a8ff9
devinchang <devinchang@126.com>
2021-07-08 00:32:25 +0800

    feat(20210708): add lc23

---
+++ LeetCode/LC23.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+
+    struct Cmp {
+        bool operator() (ListNode* a, ListNode* b) {
+            return a->val > b->val;
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Cmp> heap;
+        auto dummy = new ListNode(-1), tail = dummy;
+        for (auto l : lists) if (l) heap.push(l);
+
+        while (heap.size()) {
+            auto t = heap.top();
+            heap.pop();
+
+            tail = tail->next = t;
+            if (t->next) heap.push(t->next);
+        }
+
+        return dummy->next;
+    }
+};
